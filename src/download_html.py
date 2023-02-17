b29e05777cf2804f9ae02d26b85c0f3dae747795
FoxRLY <nevik0016@gmail.com>
2023-02-17 21:01:05 +0500

    Задокументирован модуль скачивания

--- src/download_html.py
+++ src/download_html.py
@@ -2,16 +2,54 @@ import requests
 import sys
 
 class ScheduleDownloader:
-    # Попытаться скачать страницу
+    """Модуль скачивания html страниц и получения ответов с API запросов
+
+    Удобная обертка над библиотекой requests для легкого использования 
+    ее функций в контексте сервиса "Расписание".
+
+    Поля: нет
+
+    Методы:
+    - try_download_page(url) -> tuple[str, bool]: 
+        Попытаться скачать страницу
+
+    - try_get_request(api_url, header, week_index) -> tuple[str, bool]:
+        Попытаться получить ответ с сервера
+    """
+
     def try_download_page(url: str) -> tuple[str, bool]:
+        """Попытаться скачать страницу
+
+        Скачивает html-страницу по указанному адресу и возвращает результат
+        с возможностью проверки на успех. Если второй элемент результата равен True,
+        то это победа, иначе скачать не вышло.
+
+        Аргументы:
+        - url: str - адрес html страницы для скачивания
+        """
+
         agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         response = requests.get(url, headers=agent)
         if response.ok: 
             return (response.content.decode(encoding="utf-8"), True)
         return ("", False)
     
-    # Попытаться получить недельное расписание в формате json через GET запрос
+
     def try_get_request(api_url: str, header: dict, week_index: int) -> tuple[str, bool]:
+        """Попытаться получить расписание с сервера БГТУ
+        
+        Делает GET запрос на сервер БГТУ через специальный api_url и возвращает
+        результат с возможностью проверки на успех. Если второй элемент результата
+        равен True, то это победа, иначе запрос не удался.
+
+        Аргументы:
+        - api_url: str - специальный API-URL, который использует сервер БГТУ
+
+        - header: dict - заголовок, по которому делается GET запрос
+
+        - week_index: int - индекс недели. 0 - текущая, -1 - предыдущая, 1 и далее - следующие
+        """
+
         agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         response = requests.get(api_url 
                                 + "?entity=" + header["entity"] 
