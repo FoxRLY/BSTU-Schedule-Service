088d87137f6515fa578622b8a290e31c69af220d
FoxRLY <nevik0016@gmail.com>
2023-02-20 19:58:55 +0500

    Модуль загрузки стал асинхронным

--- src/download_html.py
+++ src/download_html.py
@@ -1,13 +1,30 @@
-import requests
+"""Вспомогательный модуль для скачивания расписания
+
+Удобная обертка над библиотекой aiohttp для легкого использования 
+ее функций в контексте сервиса "Расписание".
+
+Пример использования:
+    agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
+    url = "https://google.com"
+    downloader = ScheduleDownloader(agent)
+    text, status = await downloader.try_download(url)
+    if status:
+        print(text)
+    else:
+        print("Ошибка скачивания")
+"""
+
+import aiohttp
 import sys
 
 class ScheduleDownloader:
-    """Модуль скачивания html страниц и получения ответов с API запросов
+    """Класс для скачивания html страниц и получения ответов с API запросов
 
-    Удобная обертка над библиотекой requests для легкого использования 
+    Удобная обертка над библиотекой aiohttp для легкого использования 
     ее функций в контексте сервиса "Расписание".
 
-    Поля: нет
+    Поля:
+    - headers: dict - Юзер-агент для получения корректного контента с сервера
 
     Методы:
     - try_download_page(url) -> tuple[str, bool]: 
@@ -16,8 +33,19 @@ class ScheduleDownloader:
     - try_get_request(api_url, header, week_index) -> tuple[str, bool]:
         Попытаться получить ответ с сервера
     """
+    
+    def __init__(self, headers: dict):
+        """Конструктор загрузчика 
+        
+        Настраивает заголовки, которые помогут получать корректные данные с серверов
+        
+        Аргументы:
+        - headers: dict - заголовки для HTTP запроса
+        """
 
-    def try_download_page(self, url: str) -> tuple[str, bool]:
+        self.headers = headers
+
+    async def try_download_page(self, url: str) -> tuple[str, bool]:
         """Попытаться скачать страницу
 
         Скачивает html-страницу по указанному адресу и возвращает результат
@@ -28,12 +56,12 @@ class ScheduleDownloader:
         - url: str - адрес html страницы для скачивания
         """
 
-        agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
-        response = requests.get(url, headers=agent)
-        if response.ok: 
-            return (response.content.decode(encoding="utf-8"), True)
-        return ("", False)
-    
+        async with aiohttp.ClientSession() as client:
+            response = await client.get(url, headers=self.headers)
+            if response.status == 200:
+                return (response.text(), True)
+            return ("", False)
+
 
     def try_get_request(self, api_url: str, header: dict, week_index: int) -> tuple[str, bool]:
         """Попытаться получить расписание с сервера БГТУ
@@ -45,32 +73,18 @@ class ScheduleDownloader:
         Аргументы:
         - api_url: str - специальный API-URL, который использует сервер БГТУ
 
-        - header: dict - заголовок, по которому делается GET запрос
+        - header: dict - заголовок для БГТУ API, по которому делается GET запрос (Смотри в парсер)
 
         - week_index: int - индекс недели. 0 - текущая, -1 - предыдущая, 1 и далее - следующие
         """
 
-        agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
-        response = requests.get(api_url 
+        async with aiohttp.ClientSession() as client:
+            response = await client.get(api_url 
                                 + "?entity=" + header["entity"] 
                                 + "&id=" + header["id"]
                                 + "&week=" + str(week_index)
-                                + "&device=" + header["device"], agent)
-        
-        if response.ok:
-            return (response.content.decode(encoding="utf-8"), True)
-        return ("", False)
-
-
-if __name__ == "__main__":
-    # Выходим, если нет аргументов
-    if len(sys.argv) < 2:
-        print("Ошибка: недостаточно аргументов")
-        exit()
-    url = sys.argv[1]
-    text, status = ScheduleDownloader.try_download(url)
-    if status:
-        print(text)
-    else:
-        print("Ошибка скачивания")
+                                + "&device=" + header["device"], headers=self.headers) 
+            if response.status == 200:
+                return (response.text(), True)
+            return ("", False)
 
